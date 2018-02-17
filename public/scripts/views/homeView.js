f59ffb9fcbcdc346b77868037027e44e30cc22e9
nicky evers <livelyevers@gmail.com>
2018-02-16 16:25:14 -0800

    Restyle nav component hover styles and remove redundancy

--- public/scripts/views/homeView.js
+++ public/scripts/views/homeView.js
@@ -1,10 +1,10 @@
-'use-strict';
+// 'use-strict';
 
-// nav script
-$(function() {
-  $('#teapot-logo-div').on('click', function() {
-    var $nav = $('nav');
-    $nav.fadeToggle('slow');
-    $('#teapot-logo-div').toggleClass('rotate');
-  });
-});
+// // nav script
+// $(function() {
+//   $('#teapot-logo-div').on('click', function() {
+//     var $nav = $('nav');
+//     $nav.fadeToggle('slow');
+//     $('#teapot-logo-div').toggleClass('rotate');
+//   });
+// });
